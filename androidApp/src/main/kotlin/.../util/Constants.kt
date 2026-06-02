b1cba6f6a36a39c59d2d0ced6c468fb8811eef39
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-02 13:42:36 +0630

    Create Constants.kt

---
+++ androidApp/src/main/kotlin/.../util/Constants.kt
@@ -0,0 +1,13 @@
+package com.moekyaw.aung.portfolio.util
+
+object ApiConstants {
+    const val BASE_URL = "https://api.moekyaw-aung.com/api/v1"
+    const val CONNECT_TIMEOUT = 30L
+    const val READ_TIMEOUT = 30L
+}
+
+object AppConstants {
+    const val PREF_NAME = "portfolio_prefs"
+    const val KEY_THEME = "theme_preference"
+    const val KEY_ONBOARDING_COMPLETE = "onboarding_complete"
+}
